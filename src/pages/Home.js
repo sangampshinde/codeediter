74d3bffeb29c98217dc017f5c1faa443e19c629c
sangam shinde <sangampshinde@gmail.com>
2024-05-27 14:31:58 +0530

    desing room home page css and html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,21 @@ import'./Home.css'
 
 function Home() {
   return (
-    <div className='homepagewrapper'>
-     <div className='formwrapper'>
+    <div className='homePageWrapper'>
+     <div className='formWrapper'>
       <img src='/codeediter.png' alt='codeediter logo' className='logo' />
-      <h4 className='mainlable'>Enter Your Room ID</h4>
+      <h4 className="mainLabel">Enter Your Room ID</h4>
+      <div className="inputGroup">
+      <input type='text' placeholder='ROOM ID' className="inputBox"></input>
+      <input type='text' placeholder='USER NAME' className="inputBox"></input>
+      <button className='btn joinBtn'>Join</button>
+      <span className="createInfo">
+        If you dont have invite then Create One &nbsp;
+        <a href=''className="createNewBtn">new room</a>
+      </span>
+      </div>
+      
 
-      FORM
      </div>
     </div>
   )
