ea23032541df5f63c5e33a29e5457d95e0df28fa
sangam shinde <sangampshinde@gmail.com>
2024-05-31 21:35:51 +0530

    join room functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import EditerPage from './pages/EditerPage';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import EditerPage from "./pages/EditerPage";
 
 function App() {
   return (
     <>
-    {/* roting here */}
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/editer/:roomId' element={<EditerPage/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      {/*Toster configuration notification such position size etc*/}
+      {/* it must be in global scope */}
+      <div>
+        <Toaster 
+        position="bottom-center"
+        toastOptions={{
+          sucess:{
+            theme:{
+              primary:'#03AED2'
+            }
+          }
+        }}
+        ></Toaster>
+      </div>
+
+      {/* roting here */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/editer/:roomId" element={<EditerPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,52 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
 import'./Home.css'
 
+
 function Home() {
+
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
+  const creatNewRoom = (e)=>{
+    e.preventDefault();
+    const id = uuidV4();
+    // console.log(id);
+    setRoomId(id);
+    toast.success('New room created');
+
+  }
+  const joinRoom=()=>{
+
+  }
+
+  
   return (
     <div className='homePageWrapper'>
      <div className='formWrapper'>
       <img src='/codeediter.png' alt='codeediter logo' className='logo' />
       <h4 className="mainLabel">Enter Your Room ID</h4>
       <div className="inputGroup">
-      <input type='text' placeholder='ROOM ID' className="inputBox"></input>
-      <input type='text' placeholder='USER NAME' className="inputBox"></input>
-      <button className='btn joinBtn'>Join</button>
+
+      <input 
+      type='text' 
+      placeholder='ROOM ID' 
+      className="inputBox" 
+      onChange={(e) => setRoomId(e.target.value)}
+      value={roomId}/>
+
+      <input 
+      type='text' 
+      placeholder='USER NAME' 
+      className="inputBox"  
+      onChange={(e) => setUsername(e.target.value)} 
+      value={username}/>
+
+      <button className='btn joinBtn' onClick={joinRoom}>Join</button>
       <span className="createInfo">
         If you dont have invite then Create One &nbsp;
-        <a href=''className="createNewBtn">new room</a>
+        <a onClick={creatNewRoom} href=''className="createNewBtn">new room</a>
       </span>
       </div>
       
