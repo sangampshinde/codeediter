0b49e7df5eb9e7bb285d7dfc708d6e79b903e915
sangam shinde <sangampshinde@gmail.com>
2024-05-31 22:49:29 +0530

    client component design

---
+++ src/components/Client.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Client({ username }) {
+  return (
+    <div className="client">
+      
+    </div>
+  )
+}
+
+export default Client
--- src/pages/EditerPage.js
+++ src/pages/EditerPage.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Client from '../components/Client';
 
 function EditerPage() {
+  const [clients,setClients] =useState([
+    {socketId:1,username:'sangam s',},
+    {socketId:2,username:'pavan s',}
+  ]);
+
+
+
+
   return (
     <div className="mainWrap">
     <div className="aside">
       <div className="asideInner">
         <div className="logo">
-        <img className="" src="" alt=""/>
+          <img className="logoImage logo" src="/codeediter.png" alt="code logo"/>
         </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {
+              clients.map((client)=>(
+              <Client  key={client.socketId} username={client.username}/>
+            ))
+            }
+          </div>
       </div>
     </div>
     <div className="editorWrap">Editer goes here....</div>
