2680dd67809d3f4ac7b1d1f9202295dccf1e5a3c
sangam shinde <sangampshinde@gmail.com>
2024-05-31 22:27:45 +0530

    editer page aside bar design

--- src/pages/EditerPage.js
+++ src/pages/EditerPage.js
@@ -3,6 +3,14 @@ import React from 'react'
 function EditerPage() {
   return (
     <div className="mainWrap">
+    <div className="aside">
+      <div className="asideInner">
+        <div className="logo">
+        <img className="" src="" alt=""/>
+        </div>
+      </div>
+    </div>
+    <div className="editorWrap">Editer goes here....</div>
       
     </div>
   )
